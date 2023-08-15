2bd2c21b81dca6bd35a78d697cc3364e04b5bd00
ienrum <azang0506@gmail.com>
2023-08-15 18:29:31 +0900

    feat: ProfileEditForm 컴포넌트 작성
    Profile 페이지에서 edit 모드시 사용
    
    아직 미완성 리듀서 액션을 활용해 데이터 업데이트 예정

--- components/ui/ProfileEditForm.tsx
+++ components/ui/ProfileEditForm.tsx
@@ -1,6 +1,12 @@
 import { useForm } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
 
+import { User } from "../../redux/slices/usersSlice";
+import React from "react";
+
+import { useDispatch } from "react-redux";
+import { updateUser } from "../../redux/slices/usersSlice";
+
 interface Inputs {
   name: string;
   email: string;
@@ -8,24 +14,32 @@ interface Inputs {
   passwordConfirmation: string;
 }
 
-const ProfileEditForm = (): React.ReactNode => {
+interface Props {
+  user: User;
+  editHandler: () => void;
+}
+
+const ProfileEditForm = ({ user, editHandler }: Props): React.ReactNode => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>();
 
+  const dispatch = useDispatch();
+
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
+    editHandler();
+    dispatch(updateUser(data));
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="name">Name</label>
-      <input {...register("name")} />
+      <input {...register("name", { value: user.name })} />
       {errors.name !== undefined && <span>This field is required</span>}
       <label htmlFor="email">Email</label>
-      <input type="email" {...register("email")} />
+      <input type="email" {...register("email", { value: user.email })} />
       <label htmlFor="password">Password</label>
       <input type="password" {...register("password")} />
       <label htmlFor="passwordConfirmation">Password Confirmation</label>
