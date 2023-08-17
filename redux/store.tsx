d271ac8f2a1250a174483ad1b38427f89ab23731
ienrum <azang0506@gmail.com>
2023-08-17 18:54:45 +0900

    feat: redux store 에 posts , categories 리듀서 추가

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import usersReducer from "./slices/usersSlice";
+import postsReducer from "./slices/postsSlice";
+import categoriesReducer from "./slices/categoriesSlice";
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
+    posts: postsReducer,
+    categories: categoriesReducer,
   },
 });
 
